aebad1f1ca252fbd8f5a7cf7a4cc1293c7e71edb
maayans <maayansavir@gmail.com>
2019-08-16 08:49:23 -0700

    hidden records value on response

--- app/Data.php
+++ app/Data.php
@@ -9,4 +9,6 @@ class Data extends Model
     protected $fillable = [
             'PassengerId','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked'
     ];
+
+    protected $hidden = ['created_at','updated_at','id'];
 }
