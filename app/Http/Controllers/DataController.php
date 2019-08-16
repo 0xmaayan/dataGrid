884c272e0312baadc454b602aa8e26335fb2f2df
maayans <maayansavir@gmail.com>
2019-08-16 08:50:00 -0700

    return data as array of values

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -13,8 +13,10 @@ class DataController extends Controller
 	{
 		Data::truncate();
 		Excel::import(new DataImport, $request->file('data_file'));
-
-		$data = Data::all()->toArray();
+		// map all rows as array of values
+		$data = Data::all()->map(function($item) {
+						    return array_values($item->toArray());
+						})->toArray();
 		return response()->json([
 			'data' => $data
 		]); 
