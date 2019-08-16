6e72998217371109fe867189efd05f45e1b383cb
maayans <maayansavir@gmail.com>
2019-08-16 08:50:53 -0700

    implement table grid jqwwidgets

--- resources/js/components/TableGrid.jsx
+++ resources/js/components/TableGrid.jsx
@@ -1,69 +1,69 @@
 import React, {Component} from 'react';
-
+// jxwidgets
 import 'jqwidgets-scripts/jqwidgets/styles/jqx.base.css';
 import 'jqwidgets-scripts/jqwidgets/styles/jqx.material-purple.css';
 import JqxGrid, {jqx} from 'jqwidgets-scripts/jqwidgets-react-tsx/jqxgrid';
 
-export default class TableGrid extends Component {
+class TableGrid extends Component {
 
   constructor(props) {
     super(props);
     const source =
     {
       datafields: [
-      { name: 'CompanyName', type: 'string', map: '0' },
-      { name: 'ContactName', type: 'string', map: '1' },
-      { name: 'Title', type: 'string', map: '2' },
-      { name: 'Address', type: 'string', map: '3' },
-      { name: 'City', type: 'string', map: '4' },
-      { name: 'Country', type: 'string', map: '5' }
+      { name: 'PassengerId', type: 'string', map: '0' },
+      { name: 'Pclass', type: 'string', map: '1' },
+      { name: 'Name', type: 'string', map: '2' },
+      { name: 'Sex', type: 'string', map: '3' },
+      { name: 'Age', type: 'string', map: '4' },
+      { name: 'SibSp', type: 'string', map: '5' },
+      { name: 'Parch', type: 'string', map: '6' },
+      { name: 'Ticket', type: 'string', map: '7' },
+      { name: 'Fare', type: 'string', map: '8' },
+      { name: 'Cabin', type: 'string', map: '9' },
+      { name: 'Embarked', type: 'string', map: '10' }
       ],
       datatype: 'array',
-      localdata: [
-      ['Alfreds Futterkiste', 'Maria Anders', 'Sales Representative', 'Obere Str. 57', 'Berlin', 'Germany'],
-      ['Ana Trujillo Emparedados y helados', 'Ana Trujillo', 'Owner', 'Avda. de la Constitucin 2222', 'Mxico D.F.', 'Mexico'],
-      ['Antonio Moreno Taquera', 'Antonio Moreno', 'Owner', 'Mataderos 2312', 'Mxico D.F.', 'Mexico'],
-      ['Around the Horn', 'Thomas Hardy', 'Sales Representative', '120 Hanover Sq.', 'London', 'UK'],
-      ['Berglunds snabbkp', 'Christina Berglund', 'Order Administrator', 'Berguvsvgen 8', 'Lule', 'Sweden'],
-      ['Blauer See Delikatessen', 'Hanna Moos', 'Sales Representative', 'Forsterstr. 57', 'Mannheim', 'Germany'],
-      ['Blondesddsl pre et fils', 'Frdrique Citeaux', 'Marketing Manager', '24, place Klber', 'Strasbourg', 'France'],
-      ['Blido Comidas preparadas', 'Martn Sommer', 'Owner', 'C\/ Araquil, 67', 'Madrid', 'Spain'],
-      ['Bon app', 'Laurence Lebihan', 'Owner', '12, rue des Bouchers', 'Marseille', 'France'],
-      ['Bottom-Dollar Markets', 'Elizabeth Lincoln', 'Accounting Manager', '23 Tsawassen Blvd.', 'Tsawassen', 'Canada'],
-      ['B`s Beverages', 'Victoria Ashworth', 'Sales Representative', 'Fauntleroy Circus', 'London', 'UK'],
-      ['Cactus Comidas para llelet', 'Patricio Simpson', 'Sales Agent', 'Cerrito 333', 'Buenos Aires', 'Argentina'],
-      ['Centro comercial Moctezuma', 'Francisco Chang', 'Marketing Manager', 'Sierras de Granada 9993', 'Mxico D.F.', 'Mexico'],
-      ['Chop-suey Chinese', 'Yang Wang', 'Owner', 'Hauptstr. 29', 'Bern', 'Switzerland'],
-      ['Comrcio Mineiro', 'Pedro Afonso', 'Sales Associate', 'Av. dos Lusadas, 23', 'Sao Paulo', 'Brazil'],
-      ['Consolidated Holdings', 'Elizabeth Brown', 'Sales Representative', 'Berkeley Gardens 12 Brewery', 'London', 'UK'],
-      ['Drachenblut Delikatessen', 'Sven Ottlieb', 'Order Administrator', 'Walserweg 21', 'Aachen', 'Germany'],
-      ['Du monde entier', 'Janine Labrune', 'Owner', '67, rue des Cinquante Otages', 'Nantes', 'France'],
-      ['Eastern Connection', 'Ann Devon', 'Sales Agent', '35 King George', 'London', 'UK'],
-      ['Ernst Handel', 'Roland Mendel', 'Sales Manager', 'Kirchgasse 6', 'Graz', 'Austria']
-      ]
+      localdata: props.data
     };
     this.state = {
       columns: [
-      { text: 'Company Name', datafield: 'CompanyName'},
-      { text: 'Contact Name', datafield: 'ContactName'},
-      { text: 'Contact Title', datafield: 'Title'},
-      { text: 'Address', datafield: 'Address'},
-      { text: 'City', datafield: 'City'},
-      { text: 'Country', datafield: 'Country' }
+      { text: 'Passenger Id', datafield: 'PassengerId',width:100},
+      { text: 'Pclass', datafield: 'Pclass',width:50},
+      { text: 'Name', datafield: 'Name',width:300},
+      { text: 'Sex', datafield: 'Sex',width:100},
+      { text: 'Age', datafield: 'Age',width:100},
+      { text: 'SibSp', datafield: 'SibSp',width:50 },
+      { text: 'Parch', datafield: 'Parch',width:50},
+      { text: 'Ticket', datafield: 'Ticket',width:100},
+      { text: 'Fare', datafield: 'Fare',width:100},
+      { text: 'Cabin', datafield: 'Cabin',width:100},
+      { text: 'Embarked', datafield: 'Embarked',width:50}
       ],
-      source: new jqx.dataAdapter(source)
+      source: new jqx.dataAdapter(source,{ autoBind: true }),
+      theme: 'material purple',
     }
   }
 
   render() {
     return (
       <div>
-      <JqxGrid
-      width={"100%"} columns={this.state.columns} source={this.state.source}
-      pageable={true} autoheight={true} sortable={true} altrows={true}
-      enabletooltips={true} editable={true}
-      />
+        <JqxGrid
+        width={"100%"} columns={this.state.columns} source={this.state.source} theme={this.state.theme}
+        pageable={true} autoheight={true} sortable={true} altrows={true}
+        enabletooltips={true} editable={true}
+        />
+      <div className="row">
+        <div className="col-md-6">
+          <h1>Pie</h1>
+        </div>
+        <div className="col-md-6">
+          <h1>Bar</h1>
+        </div>
+      </div>
       </div>
       );
   }
 }
+
+export default TableGrid;
